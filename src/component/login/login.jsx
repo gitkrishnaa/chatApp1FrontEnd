b10d6b9e068cdecf39b74be3811e4ca4c4dceec6
krishna kumar <kri821084@gmail.com>
2023-06-09 18:33:00 +0530

    login,signup pafe working sucessful 2.login and signup fuction by api using node workin sucessfully 3. note-i have used the axios package in react

--- src/component/login/login.jsx
+++ src/component/login/login.jsx
@@ -1,18 +1,39 @@
 
 import "./login.css"
-
+import { useState } from "react"
+import axios from 'axios';
 function App() {
+    const [email,setEmail]=useState("a@b.c")
+    const [password,setPassword]=useState("123")
+        const submitFun=async()=>{
+       
+            try {
+            const resp2= await axios.post("http://localhost:4000/user/login",{
+            user_email:email,
+            user_password:password,  
+            })
+            console.log(resp2)
+            alert(resp2.data.message)
+        }
+     catch (error) {
+        alert("front end error ,see in console")
+        console.log("errot i login-",error)
+    }
+}
     return (
 <div className="div_main">
 <div className="div1">
    
    
-    <div  className="input_div"> <input type="text"  placeholder="Email"/>  </div>
+    <div  className="input_div"> <input type="text"  placeholder="Email" value={email} onChange={(e)=>{
+setEmail(e.target.value)
+    }}/>  </div>
 
-    <div className="input_div"> <input type="password" placeholder="Password" />  </div>
+    <div className="input_div"> <input type="password" placeholder="Password"  value={password} onChange={(e)=>{
+password(e.target.value)}}/>  </div>
     
 <div className="input_div">
-    <button className="button1">submit</button>
+    <button className="button1" onClick={submitFun}> submit</button>
     </div>
 </div>
 </div>
--- src/component/signup/signUp.jsx
+++ src/component/signup/signUp.jsx
@@ -1,19 +1,53 @@
 
 import "./signUp.css"
-
+import axios from "axios"
+const {useState}=require("react")
 function App() {
+    const [name,setName]=useState("Krishna")
+    const [email,setEmail]=useState("a@b.c")
+    const [password,setPassword]=useState("123")
+    const [mobile,setMobile]=useState(1234567890)
+
+
+    const submitFun=async()=>{
+       
+
+        const resp2= await axios.post("http://localhost:4000/user/signup",{
+            user_name:name,
+            user_email:email,
+            user_password:password,
+            user_mobile:mobile
+
+        })
+        console.log(resp2)
+        alert(resp2.data.message)
+    }
     return (
 <div className="div_main">
 <div className="div1">
    
-<div  className="input_div"> <input type="text"  placeholder="Name"/>  </div>
+<div  className="input_div"> <input type="text" value={name} onChange={(e)=>{
+setName(e.target.value);
 
-    <div  className="input_div"> <input type="text"  placeholder="Email"/>  </div>
+}}  placeholder="Name"/>  </div>
 
-    <div className="input_div"> <input type="password" placeholder="Password" />  </div>
+<div  className="input_div"> <input type="number" value={mobile} onChange={(e)=>{
+setMobile(e.target.value);
+
+}}  placeholder="Mobile No."/>  </div>
+
+    <div  className="input_div"> <input type="text"  placeholder="Email" value={email} onChange={(e)=>{
+setEmail(e.target.value);
+
+}}  />  </div>
+
+    <div className="input_div"> <input type="password" placeholder="Password" value={password} onChange={(e)=>{
+setPassword(e.target.value);
+
+}}  />  </div>
     
 <div className="input_div">
-    <button className="button1">submit</button>
+    <button className="button1" onClick={submitFun}>submit</button>
     </div>
 </div>
 </div>
